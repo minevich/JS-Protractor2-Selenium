649ed5e3bf1900e8d12774be1a0fba9e95915684
Alex Minevich <alex.minevich@vervemobile.com>
2016-09-28 21:02:50 -0700

    update conf.js

--- conf.js
+++ conf.js
@@ -1,7 +1,7 @@
 // conf.js
 exports.config = {
-  sauceUser: process.env.SAUCE_USERNAME,
-  sauceKey: process.env.SAUCE_ACCESS_KEY,
+  // sauceUser: process.env.SAUCE_USERNAME,
+  // sauceKey: process.env.SAUCE_ACCESS_KEY,
 
   //seleniumAddress: 'http://ondemand.saucelabs.com:80/wd/hub',
   specs: ['specs/*spec.js'],
@@ -12,29 +12,29 @@ exports.config = {
       var caps = browser.getCapabilities()
   },
 
-  multiCapabilities: [{
-    browserName: 'firefox',
-    version: '32',
-    platform: 'OS X 10.10',
-    name: "firefox-tests",
-    shardTestFiles: true,
-    maxInstances: 25
-  }, {
-    browserName: 'chrome',
-    version: '41',
-    platform: 'Windows 7',
-    name: "chrome-tests",
-    shardTestFiles: true,
-    maxInstances: 25
-  }],
+    // multiCapabilities: [{
+    //   browserName: 'firefox',
+    //   version: '32',
+    //   platform: 'OS X 10.10',
+    //   name: "firefox-tests",
+    //   shardTestFiles: true,
+    //   maxInstances: 25
+    // }, {
+    //   browserName: 'chrome',
+    //   version: '41',
+    //   platform: 'Windows 7',
+    //   name: "chrome-tests",
+    //   shardTestFiles: true,
+    //   maxInstances: 25
+    // }],
 
-  onComplete: function() {
+    // onComplete: function() {
 
-    var printSessionId = function(jobName){
-      browser.getSession().then(function(session) {
-        console.log('SauceOnDemandSessionID=' + session.getId() + ' job-name=' + jobName);
-      });
-    }
-    printSessionId("Insert Job Name Here");
-  }
+    //   var printSessionId = function(jobName){
+    //     browser.getSession().then(function(session) {
+    //       console.log('SauceOnDemandSessionID=' + session.getId() + ' job-name=' + jobName);
+    //     });
+    //   }
+    //   printSessionId("Insert Job Name Here");
+    // }
 }
